36f8346ed5d8993efcbacfe1d538bcfa034664a4
Darya Papko <dashk1992@gmail.com>
2018-04-10 21:39:33 +0300

    Fix test

--- src/test/java/com/eshchepetov/learnenglish/learnenglish/LearnEnglishApplicationTests.java
+++ src/test/java/com/eshchepetov/learnenglish/learnenglish/LearnEnglishApplicationTests.java
@@ -6,11 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = LearnEnglishApplication.class)
 public class LearnEnglishApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
